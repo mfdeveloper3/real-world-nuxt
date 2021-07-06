e8aae402baca02a674d0a7fdf9689c05c7cdd6fe
mfdeveloper3 <managerfashion.developer3@gmail.com>
2021-07-06 15:28:15 -0300

    end of lesson 4

---
+++ pages/create.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <h1>Create</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  head() {
+    return {
+      title: 'Create an event',
+    }
+  }
+}
+</script>
---
+++ pages/index.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <h1>Eventos</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  head() {
+    return {
+      title: 'Event Listing - Real world events',
+      meta: [
+        {
+          hid: 'description',
+          name: 'description',
+          content: 'pipipipopopo palmeiras 2 santos 0'
+        }
+      ]
+    }
+  }
+}
+</script>
